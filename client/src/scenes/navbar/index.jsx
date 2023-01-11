77fc5fe3c538155df4f00ff248f9b3b6f542893f
Mounir <polomnr1@gmail.com>
2023-01-11 13:11:09 +0100

    start working on the navbar

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -1,3 +1,32 @@
+import {useState} from "@types/react";
+import {
+    Box,
+    IconButton,
+    InputBase,
+    Typography,
+    Select,
+    MenuItem,
+    FormControl,
+    useTheme,
+    useMediaQuery
+} from "@mui/material";
+
+import {
+    Search,
+    Message,
+    DarkMode,
+    LightMode,
+    Notifications,
+    Help,
+    Menu,
+    Close
+} from "@mui/icons-material";
+import {useDispatch, useSelector} from "react-redux";
+import { setMode, setLogout } from "../../state/index.js";
+import { useNavigate } from "react-router-dom";
+import FlexBetween from "../../styles/FlexBetween.jsx";
+
+
 function NavBar() {
     return (
         <div>
