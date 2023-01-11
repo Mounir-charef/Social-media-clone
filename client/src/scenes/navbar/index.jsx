75e32a21c22fc9af0a696e9d45f7a817cbfaf085
Mounir <polomnr1@gmail.com>
2023-01-11 14:14:00 +0100

    Mobile Nav done

--- client/src/scenes/navbar/index.jsx
+++ client/src/scenes/navbar/index.jsx
@@ -122,7 +122,75 @@ function NavBar() {
                         <Menu />
                     </IconButton>
                 )}
-
+                {/*mobile nav*/}
+                {isMobileMenuToggled && isMobileMenuToggled && (
+                    <Box
+                        position="fixed"
+                        right="0"
+                        bottom="0"
+                        zIndex="10"
+                        maxWidth="500px"
+                        minwidth="300px"
+                        backgroundColor={background}
+                    >
+                        {/*Close Icon*/}
+                        <Box display='flex' justifyContent='flex-end' p='1rem'>
+                            <IconButton
+                                onClick={() => setIsMobileMenuToggled(!isMobileMenuToggled)}
+                            >
+                                <Close/>
+                            </IconButton>
+                        </Box>
+                        {/*Menu Items*/}
+                        <FlexBetween
+                            display='flex'
+                            flexDirection='column'
+                            justifyContent='center'
+                            alignItems='center'
+                            gap='3rem'
+                        >
+                            <IconButton onClick={() => dispatch(setMode())}>
+                                {theme.palette.mode === "dark" ? (
+                                    <DarkMode sx={{fontSize: "25px"}} />
+                                ): (
+                                    <LightMode sx={{color: dark, fontSize: "25px"}} />
+                                )}
+                            </IconButton>
+                            <Message sx={{fontSize: "25px"}} />
+                            <Notifications sx={{fontSize: "25px"}} />
+                            <Help sx={{fontSize: "25px"}} />
+                            <FormControl variant="standard" value={fullName}>
+                                <Select
+                                    value={fullName}
+                                    sx={{
+                                        background: neutralLight,
+                                        width: '150px',
+                                        borderRadius: '0.25rem',
+                                        p: '0.25rem 1rem',
+                                        "& .MuiSvgIcon-root": {
+                                            pr: '0.25rem',
+                                            width: '3rem',
+                                        },
+                                        "& .MuiSelect-select": {
+                                            background: neutralLight,
+                                        }
+                                    }}
+                                    input={<InputBase />}
+                                >
+                                    <MenuItem value={fullName}>
+                                        <Typography>
+                                            {fullName}
+                                        </Typography>
+                                    </MenuItem>
+                                    <MenuItem onClick={() => dispatch(setLogout())}>
+                                        Log Out
+                                    </MenuItem>
+                                </Select>
+                            </FormControl>
+                        </FlexBetween>
+                    </Box>
+                )
+                }
             </FlexBetween>
         </FlexBetween>
     );
