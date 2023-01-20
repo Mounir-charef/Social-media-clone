d0c16dd64c00fa3d1567a71354c7f5b84e1dd50a
Mounir <polomnr1@gmail.com>
2023-01-20 21:07:58 +0100

    auto login with token

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -20,7 +20,7 @@ function App() {
         <ThemeProvider theme={theme}>
             <CssBaseline />
             <Routes>
-                <Route path="/" element={<LoginPage />} />
+                <Route path="/" element={!isAuth? <LoginPage /> : <Navigate to='/home' />} />
                 <Route path="/home" element={isAuth? <HomePage /> : <Navigate to='/' />} />
                 <Route path="/profile/:userId" element={isAuth? <ProfilePage /> : <Navigate to='/' />} />
             </Routes>
